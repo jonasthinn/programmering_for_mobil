9a1eed433fd1d36772b46dec81d29d3be883ec26
jonasthinn <jth036@post.uit.no>
2023-01-25 18:13:01 +0100

    Game is implemented

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -2,17 +2,82 @@ package com.example.tictactoe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import com.example.tictactoe.databinding.ActivityMainBinding
-import androidx.viewbinding.ViewBinding
 
 class MainActivity : AppCompatActivity() {
+    var playerOnesTurn: Boolean = true
+    lateinit var playerOne: TTTPlayer
+    lateinit var playerTwo: TTTPlayer
+    lateinit var board: TTT_Board
+    lateinit var game: TicTacToeGame
+    lateinit var binding: ActivityMainBinding
+    var weHaveAWinner: Boolean = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
+        playerOne = TTTPlayer("X")
+        playerTwo = TTTPlayer("O")
+        board = TTT_Board(3, 3)
+        game = TicTacToeGame(board, listOf(playerOne, playerTwo))
+
+        binding.playerOne.text = "Player One: '${playerOne.playerSymbol}'\nScore: ${playerOne.score}"
+        binding.playerTwo.text = "Player Two: '${playerOne.playerSymbol}'\nScore: ${playerTwo.score}"
+
+        enableButtons(true)
+        binding.playAgain.setOnClickListener() { restartGame() }
 
 
     }
+
+    fun onClick(view: View) {
+        val button = view as Button
+        val text = if (playerOnesTurn) playerOne.playerSymbol else playerTwo.playerSymbol
+        if (button.text == " ") {
+            button.text = text
+            playerOnesTurn = !playerOnesTurn
+            var buttonId: Char = resources.getResourceEntryName(button.id).last()
+            game.ttt_board.makeMove(buttonId, text)
+        }
+        if (game.checkForWin()) {
+            var winner: TTTPlayer = if (playerOnesTurn) playerTwo else playerOne
+            winner.score += 1
+            binding.playerOne.text = "Player One: '${playerOne.playerSymbol}'\nScore: ${playerOne.score}"
+            binding.playerTwo.text = "Player Two: '${playerTwo.playerSymbol}'\nScore: ${playerTwo.score}"
+            Toast.makeText(this, "jippipii", Toast.LENGTH_LONG).show()
+            enableButtons(false)
+        }
+    }
+
+    fun enableButtons(enableOrDisable: Boolean) {
+        val layout: ViewGroup = binding.gl
+        for (i in 0 until layout.childCount) {
+            if (layout.getChildAt(i) is Button) {
+                if (enableOrDisable) {
+                    layout.getChildAt(i).setOnClickListener() { onClick(it) }
+                } else layout.getChildAt(i).isClickable = false
+            }
+        }
+    }
+
+    fun restartGame() {
+        val layout: ViewGroup = binding.gl
+        playerOnesTurn = true
+        board = TTT_Board(3, 3)
+        game = TicTacToeGame(board, listOf(playerOne, playerTwo))
+        enableButtons(true)
+        for (i in 0 until layout.childCount) {
+            if (layout.getChildAt(i) is Button) {
+                (layout.getChildAt(i) as Button).setText(" ")
+            }
+        }
+    }
 }
+
+
--- app/src/main/java/com/example/tictactoe/TTT_Board.kt
+++ app/src/main/java/com/example/tictactoe/TTT_Board.kt
@@ -2,11 +2,24 @@ package com.example.tictactoe
 
 class TTT_Board(row: Int = 3, columns: Int = 3) : Board(row, columns) {
 
+    // Metoden tar en field: Int, i stedet for feks "row" og "column". Dette for å lettere
+    // implementere i appen
+    fun makeMove(field: Char, playerChar: String) {
+        when (field){
+            '1' -> board[0][0] = playerChar
+            '2' -> board[0][1] = playerChar
+            '3' -> board[0][2] = playerChar
+            '4' -> board[1][0] = playerChar
+            '5' -> board[1][1] = playerChar
+            '6' -> board[1][2] = playerChar
+            '7' -> board[2][0] = playerChar
+            '8' -> board[2][1] = playerChar
+            '9' -> board[2][2] = playerChar
 
-    fun makeMove(row: Int, column: Int, playerChar: String) {
-        board[row][column] = playerChar
+        }
     }
 
+    operator fun get(i: Int, j: Int) = board[i][j]
     override fun toString(): String = """
             |
             |      ${board[0]}
@@ -14,6 +27,8 @@ class TTT_Board(row: Int = 3, columns: Int = 3) : Board(row, columns) {
             |      ${board[2]}
             |
         """.trimMargin()
+
+
 }
 
 fun main() {
